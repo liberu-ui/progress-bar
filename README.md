972dd110d3eea0f2fba1b1fa0365d819d0029811
mishu <mishu@xtelecom.ro>
2019-02-16 14:33:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Progress Bar
+
+[![License](https://poser.pugx.org/laravel-enso/progress-bar/license)](https://packagist.org/packages/laravel-enso/progress-bar)
+[![Latest Stable Version](https://poser.pugx.org/laravel-enso/progress-bar/version)](https://packagist.org/packages/laravel-enso/progress-bar)
+
+Unopinionated Progress Bar
+
+### Features
+
+- soon
+
+### Configuration & Usage
+
+- soon
+
+### Contributions
+
+are welcome. Pull requests are great, but issues are good too.
+
+### License
+
+This package is released under the MIT license.
---
+++ src/ProgressBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div :style="style"/>
+</template>
+
+<script>
+export default {
+    props: {
+        color: {
+            type: String,
+            default: '#f44336',
+        },
+        thickness: {
+            type: Number,
+            default: 2,
+            validator: v => v > 0,
+        },
+        opacity: {
+            type: Number,
+            default: 1,
+            validator: v => v > 0 && v <= 1,
+        },
+        progress: {
+            type: Number,
+            required: true,
+        },
+        position: {
+            type: String,
+            default: 'absolute',
+            validator: v => ['absolute', 'fixed'].includes(v),
+        },
+    },
+
+    computed: {
+        style() {
+            return {
+                position: this.position,
+                left: '0px',
+                top: '0px',
+                height: `${this.thickness}px`,
+                width: `${this.progress}%`,
+                opacity: this.opacity,
+                backgroundColor: this.color,
+                boxShadow: `0 0 5px ${this.color}`,
+            };
+        },
+    },
+};
+</script>
